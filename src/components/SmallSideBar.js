299e082ec26b0500e3f0b8fb893e2a7046534824
Hoang Van Binh <binhhvFX12117@funix.edu.vn>
2022-06-14 15:46:14 +0700

    106. Hết phần smallsidebar

--- src/components/SmallSideBar.js
+++ src/components/SmallSideBar.js
@@ -8,20 +8,47 @@ import links from '../utils/link';
 import Logo from './Logo';
 
 const SmallSideBar = () => {
+    const { showSlidebar, toggleSlidebar } = useAppContext();
     return (
         <Wrapper>
-            <div className="sidebar-container show-sidebar">
+            <div
+                className={
+                    showSlidebar
+                        ? 'sidebar-container show-sidebar'
+                        : 'sidebar-container'
+                }
+            >
                 <div className="content">
                     <button
                         type="button"
                         className="close-btn"
-                        onClick={() => console.log('toogle sidebar')}
+                        onClick={toggleSlidebar}
                     >
                         <FaTimes />
                     </button>
                     <header>
                         <Logo />
                     </header>
+                    <div className="nav-links">
+                        {links.map((link) => {
+                            const { id, text, path, icon } = link;
+                            return (
+                                <NavLink
+                                    to={path}
+                                    key={id}
+                                    onClick={toggleSlidebar}
+                                    className={({ isActive }) =>
+                                        isActive
+                                            ? 'nav-link active'
+                                            : 'nav-link'
+                                    }
+                                >
+                                    <span className="icon">{icon}</span>
+                                    {text}
+                                </NavLink>
+                            );
+                        })}
+                    </div>
                 </div>
             </div>
         </Wrapper>
