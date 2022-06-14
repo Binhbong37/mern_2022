ddc56ced56bb69bc8469b252067e4cadbe026b5c
Hoang Van Binh <binhhvFX12117@funix.edu.vn>
2022-06-14 15:33:05 +0700

    Thêm phần giống với modal trong smallsidebar

--- src/components/SmallSideBar.js
+++ src/components/SmallSideBar.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import { FaTimes } from 'react-icons/fa';
+import { NavLink } from 'react-router-dom';
+
 import Wrapper from '../assets/wrappers/SmallSidebar';
+import { useAppContext } from '../context/appContext';
+import links from '../utils/link';
+import Logo from './Logo';
 
 const SmallSideBar = () => {
     return (
         <Wrapper>
-            <h3>Small Side Bar</h3>
+            <div className="sidebar-container show-sidebar">
+                <div className="content">
+                    <button
+                        type="button"
+                        className="close-btn"
+                        onClick={() => console.log('toogle sidebar')}
+                    >
+                        <FaTimes />
+                    </button>
+                    <header>
+                        <Logo />
+                    </header>
+                </div>
+            </div>
         </Wrapper>
     );
 };
