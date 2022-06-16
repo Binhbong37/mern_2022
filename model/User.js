c93bbaca9edb3c81b0ac82aa6e84b387580e956f
Hoang Van Binh <binhhvFX12117@funix.edu.vn>
2022-06-16 15:54:49 +0700

    Thêm logic cho phần update, chú ý isModified trong kiểm tra sự thay đổi các ô

--- model/User.js
+++ model/User.js
@@ -38,12 +38,13 @@ const UserSchema = new Schema({
         type: String,
         required: [true, 'Plz fill input password'],
         minlength: 5,
-        maxlength: 20,
         trim: true,
+        select: false,
     },
 });
 
 UserSchema.pre('save', async function () {
+    if (!this.isModified('password')) return;
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
 });
