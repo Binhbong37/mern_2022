a9fc2fbc227fd64daf4c21dfa2db5fdf11d89f7d
Hoang Van Binh <binhhvFX12117@funix.edu.vn>
2022-06-20 16:49:49 +0700

    Refactor phần trả dữ liệu của stats

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,7 +1,8 @@
 import mongoose from 'mongoose';
 import { StatusCodes } from 'http-status-codes';
-import Job from '../model/Job.js';
+import moment from 'moment';
 
+import Job from '../model/Job.js';
 import { BadRequest, NotFound } from '../errors/index.js';
 import checkPermission from '../utils/checkPermission.js';
 
@@ -90,7 +91,33 @@ const showStats = async (req, res) => {
         declined: stats.declined || 0,
         interview: stats.interview || 0,
     };
-    let monthApplycation = [];
+    let monthApplycation = await Job.aggregate([
+        { $match: { createdBy: mongoose.Types.ObjectId(req.user.userId) } },
+        {
+            $group: {
+                _id: {
+                    year: { $year: '$createdAt' },
+                    month: { $month: '$createdAt' },
+                },
+                count: { $sum: 1 },
+            },
+        },
+        { $sort: { '_id.year': -1, '_id.month': -1 } },
+        { $limit: 6 },
+    ]);
+    monthApplycation = monthApplycation
+        .map((item) => {
+            const {
+                _id: { year, month },
+                count,
+            } = item;
+            const date = moment()
+                .month(month - 1)
+                .year(year)
+                .format('MMM Y');
+            return { count, date };
+        })
+        .reverse();
     res.status(StatusCodes.OK).json({ defaulStats, monthApplycation });
 };
 
